5940c99dcfc920e4c3adf58e568626cbe4f95de7
FozlyRabbi <rabbihasan151537@gmail.com>
2021-05-02 10:47:23 +0600

    prepearing for heruku

---
+++ server.js
@@ -0,0 +1,15 @@
+const jsonServer = require("json-server");
+
+const server = jsonServer.create();
+
+const router = jsonServer.router("./server/db.json");
+
+const middlewares = jsonServer.defaults();
+
+const port = process.env.PORT || 3200;
+
+server.use(middlewares);
+
+server.use(router);
+
+server.listen(port);
